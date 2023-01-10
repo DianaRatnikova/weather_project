899f1f4ceb6ee0472b664d5a356f49bc3fc3d857
DianaRatnikova <Diana_r@bk.ru>
2023-01-10 21:47:26 +0300

    Added Debug

--- server.py
+++ server.py
@@ -6,7 +6,10 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     weather = weather_by_city("Moscow, Russia")
-    return f"Сейчас {weather['temp_C']}, ощущается как {weather['FeelsLikeC']}"
+    if weather:
+        return f"Сейчас {weather['temp_C']}, ощущается как {weather['FeelsLikeC']}"
+    else:
+        return "Сервис погоды временно недоступен"
 
 if __name__=="__main__":
-    app.run()
+    app.run(debug = True)
