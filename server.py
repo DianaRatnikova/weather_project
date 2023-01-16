476237ca5b46a8879dbeaf575937591cbdd0d055
DianaRatnikova <Diana_r@bk.ru>
2023-01-16 22:16:29 +0300

    Added Bootstrap

--- server.py
+++ server.py
@@ -1,15 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 from weather import weather_by_city
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    page_title = "Новости Python"
     weather = weather_by_city("Moscow, Russia")
-    if weather:
-        return f"Сейчас {weather['temp_C']}, ощущается как {weather['FeelsLikeC']}"
-    else:
-        return "Сервис погоды временно недоступен"
+  #  if weather:
+ #       weather_text =  f"Сейчас {weather['temp_C']}, ощущается как {weather['FeelsLikeC']}"
+ #   else:
+  #      weather_text =   "Сервис погоды временно недоступен"
+    return render_template("index.html", weather=weather, page_title=page_title)
+
 
 if __name__=="__main__":
     app.run(debug = True)
