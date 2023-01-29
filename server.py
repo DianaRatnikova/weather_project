3f0660e148b2aeee9eb63dc1cd0dbdbbd8ae5d84
DianaRatnikova <Diana_r@bk.ru>
2023-01-29 21:55:41 +0300

    Added news with url, title and date

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+from python_org_news import get_python_news
 from flask import Flask, render_template
 from weather import weather_by_city
 
@@ -7,11 +8,12 @@ app = Flask(__name__)
 def index():
     page_title = "Новости Python"
     weather = weather_by_city("Moscow, Russia")
+    news_list = get_python_news()
   #  if weather:
  #       weather_text =  f"Сейчас {weather['temp_C']}, ощущается как {weather['FeelsLikeC']}"
  #   else:
   #      weather_text =   "Сервис погоды временно недоступен"
-    return render_template("index.html", weather=weather, page_title=page_title)
+    return render_template("index.html", weather=weather, page_title=page_title, news_list = news_list)
 
 
 if __name__=="__main__":
