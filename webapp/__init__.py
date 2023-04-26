259255006266a2d30351289686c54c75f5696bbf
DianaRatnikova <Diana_r@bk.ru>
2023-04-26 13:39:01 +0300

    Showing news from database, not from site

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,29 +1,7 @@
-from webapp.python_org_news import get_python_news
+from webapp.python_org_news import get_python_news_without_database #можно убрать
 from flask import Flask, render_template
 from webapp.weather import weather_by_city
-from webapp.models import db
-
-def create_app_old():
-  app = Flask(__name__)
-  app.config.from_pyfile('config.py')
-  db.init_app(app)
-
-  with app.app_context():
-    db.create_all()
-
-  @app.route("/")
-  def index():
-    page_title = "Новости Python"
-    weather = weather_by_city(app.config["WEATHER_DEFAULT_CITY"])
-    news_list = get_python_news()
-      #  if weather:
-    #       weather_text =  f"Сейчас {weather['temp_C']}, ощущается как {weather['FeelsLikeC']}"
-    #   else:
-      #      weather_text =   "Сервис погоды временно недоступен"
-    return render_template("index.html", weather=weather, page_title=page_title, news_list = news_list)
-
-  return app
-
+from webapp.models import db, News
 
 
 def create_app():
@@ -40,7 +18,9 @@ def create_app():
     def index():
       page_title = "Новости Python"
       weather = weather_by_city(app.config["WEATHER_DEFAULT_CITY"])
-      news_list = get_python_news()
+      news_list = get_python_news_without_database() #старый вариант - парсинг с сайта
+      news_list = News.query.order_by(News.published.desc()).all() # добавление из базы данных с сортировкой по дате
+
       #  if weather:
     #       weather_text =  f"Сейчас {weather['temp_C']}, ощущается как {weather['FeelsLikeC']}"
     #   else:
--- webapp/python_org_news.py
+++ webapp/python_org_news.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime
+from webapp.models import db, News
 
 def get_html(url):
     try:
@@ -9,6 +11,33 @@ def get_html(url):
     except(requests.RequestException, ValueError):
         print('Сетевая ошибка')
         return False
+
+
+def get_python_news_without_database():
+    html = get_html("https://www.python.org/blogs/")
+    if html:
+        soup = BeautifulSoup(html, 'html.parser')
+        all_news = soup.find('ul', class_='list-recent-posts').findAll('li')
+        result_news = []
+        for news in all_news:
+            title = news.find('a').text
+            url = news.find('a')['href']
+            published =news.find('time').text
+            try:
+                published = datetime.strptime(published, '%Y-%m-%d')
+            except(ValueError):
+                published = datetime.now()
+       # print(f"{title = }")
+       # print(f"{url = }")
+       # print(f"{published = }")
+
+            result_news.append({
+                "title": title, 
+                "url": url, 
+                "published": published
+            })
+        return result_news
+    return False
 
 
 def get_python_news():
@@ -21,18 +50,22 @@ def get_python_news():
             title = news.find('a').text
             url = news.find('a')['href']
             published =news.find('time').text
-
+            try:
+                published = datetime.strptime(published, '%Y-%m-%d')
+            except(ValueError):
+                published = datetime.now()
+            save_news(title, url, published)
        # print(f"{title = }")
-       # print(f"{url = }")
-       # print(f"{published = }")
 
-            result_news.append({
-                "title": title, 
-                "url": url, 
-                "published": published
-            })
-        return result_news
-    return False
+
+# сохранение в базу данных
+def save_news(title, url, published):
+    #проверка, есть ли считанный url уже в базе данных 
+    news_exists = News.query.filter(News.url == url).count()
+    if not news_exists:
+        new_news = News(title=title, url=url, published=published)
+        db.session.add(new_news)
+        db.session.commit()
 
 
 if __name__ == "__main__":
