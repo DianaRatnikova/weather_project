454326a2b6abd7e346aa0320043f9a3b089b0507
DianaRatnikova <Diana_r@bk.ru>
2023-04-27 14:09:12 +0300

    Added login/logout

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,8 +1,10 @@
 from webapp.python_org_news import get_python_news_without_database #можно убрать
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for
+from flask_login import LoginManager, login_user, logout_user
 from webapp.weather import weather_by_city
-from webapp.models import db, News
+from webapp.models import db, News, User
 
+from webapp.forms import LoginForm
 
 def create_app():
     app = Flask(__name__)
@@ -13,6 +15,16 @@ def create_app():
 
     with app.app_context():
         db.create_all()
+
+# добавление фласк-логина
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = 'login'
+  
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(user_id)
+
 
     @app.route("/")
     def index():
@@ -27,7 +39,36 @@ def create_app():
       #      weather_text =   "Сервис погоды временно недоступен"
       return render_template("index.html", weather=weather, page_title=page_title, news_list = news_list)
 
+    @app.route('/login')
+    def login():
+        title = "Авторизация"
+        login_form = LoginForm()
+        return render_template('login.html', page_title=title, form=login_form)
+
+    @app.route('/process-login', methods=['POST'])
+    def process_login():
+      form = LoginForm()
+      # если ошибки не возникло, можно запросить пользователя из базы данных
+      if form.validate_on_submit():
+          user = User.query.filter_by(username=form.username.data).first()
+          if user and user.check_password(form.password.data):
+              login_user(user)
+              flash('Вы вошли на сайт')
+              return redirect(url_for('index'))
+      flash('Неправильное имя пользователя или пароль')
+      return redirect(url_for('login'))
+
+    @app.route('/logout')
+    def logout():
+        logout_user()
+        flash('Вы успешно разлогинились')
+        return redirect(url_for('index'))
+
     return app
+
+
+
+
 
 
 if __name__=="__main__":
